cc3e4bfcc2014355e48ce8f51bb94f446d47a154
Tom LeZotte <tom@lezotte.net>
2015-04-22 22:52:35 -0500

    added more features to the install script

--- install.sh
+++ install.sh
@@ -1,19 +1,59 @@
 #!/bin/bash
 
-echo -n "Enter a Vagrant Box (http://bit.ly/rantboxes) [chef/centos-6.6]: "
+# TODO: have this script update Vagrantfile with variables
+
+defaultbox="chef/centos-6.6"
+defaultboxip="192.168.10.1"
+defaultdomain="vagrant.com"
+
+clear
+
+echo -n "Enter a Vagrant Box (http://bit.ly/rantboxes) [$defaultbox]: "
 read box
 
+
+#
+# -- box section --
+#
 # set default is enter is pressed
 if [ -z "$box" ];
 then
-    box="chef/centos-6.6"
+    box=$defaultbox
 fi
 
 # split vagrant box string
 PROV=${box%/*}
 OS=${box##*/}
+boxhostname=${OS//[^[:alnum:]]/}.$defaultdomain
 
+# check to see if directory exists
+if [ -d "$OS" ];
+then
+    printf "\n** The directory $OS already exists **\n"
+    exit
+fi
 
+#
+# -- ip address section --
+#
+echo -n "Enter an IP address for $boxhostname [$defaultboxip]: "
+read boxip
+
+# set default if enter is pressed
+if [ -z "$boxip" ];
+then
+    boxip=$defaultboxip
+fi
+# check hosts file for ip or dns name
+checkhosts=$(grep '$boxip\|$OS' /etc/hosts)
+if [ ${#checkhosts} -ne 0 ];
+then
+    printf "\n** $boxip or $boxhostname already exists in /etc/hosts **\n"
+    exit
+fi
+
+echo "$boxip $boxhostname" >> /etc/hosts
+exit
 # Check for Homebrew,
 # Install if we don't have it
 if test ! $(which brew); then
@@ -26,8 +66,15 @@ brew install brew-cask
 brew install ansible
 
 # Install cask software
-brew cask install vagrant
-brew cask install virtualbox
+if [ ! -d "/opt/homebrew-cask/Caskroom/vagrant" ];
+then
+    brew cask install vagrant
+fi
+
+if [ ! -d "/Applications/VirtualBox.app" ];
+then
+    brew cask install virtualbox
+fi
 
 brew cleanup
 
